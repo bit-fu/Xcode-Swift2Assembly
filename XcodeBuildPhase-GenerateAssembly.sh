3ca2b3f519735803ccde0fa1641a527cea41c607
Ulrich Singer <bit.fu@gmx.net>
2022-05-30 09:29:29 +0200

    Avoiding errors with generating assembly for MacCatalyst.
    I don't know how to do that yet, can't find documentation.

--- XcodeBuildPhase-GenerateAssembly.sh
+++ XcodeBuildPhase-GenerateAssembly.sh
@@ -7,9 +7,10 @@ targetRoot="${SRCROOT}/${TARGET_NAME}"
 # Dump the Xcode env to a file.
 #printenv | sort > "${targetRoot}/xcode.${ACTION}.${TARGET_NAME}.${PLATFORM_NAME}.${CONFIGURATION}-env.sh"
 
-# Find out the SDK version to build for.
-sdkVersion=$(echo "${SDK_NAME}" | grep -o '[0-9].*$')
-
-# Generate assembly for target's Swift sources.
-"${HOME}/bin/swift2asm" -P "${PLATFORM_FAMILY_NAME}" -A "${PLATFORM_PREFERRED_ARCH}" -V "${sdkVersion}" -B "${SWIFT_OBJC_BRIDGING_HEADER}" "${targetRoot}"
-
+if [[ -z "${IS_MACCATALYST}" ]]
+then
+  # Generate assembly for target's Swift sources.
+  ${HOME}/bin/swift2asm -P "${PLATFORM_FAMILY_NAME}" -A "${PLATFORM_PREFERRED_ARCH}" -V "${SDK_VERSION}" -B "${SWIFT_OBJC_BRIDGING_HEADER}" "${targetRoot}"
+else
+  echo "Skipping assembly generation for MacCatalyst."
+fi
